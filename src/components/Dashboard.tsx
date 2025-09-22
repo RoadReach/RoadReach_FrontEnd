77584db947d0e0bb4a047b2f40ee110e439c5372
Phani3458 <prasadch0706@gmail.com>
2025-09-22 17:53:26 -0500

    Add in profile dropdown css

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import './Dashboard.css';
-
+import { useSessionTimeout } from "../hooks/useSessionTimeout";
 
 const Dashboard: React.FC = () => {
+  useSessionTimeout({onLogout: () => {
+      localStorage.clear();
+      sessionStorage.clear();
+      window.location.href = "/login";
+    }
+  });
 
   const firstName = localStorage.getItem("firstname") || "User";
 
